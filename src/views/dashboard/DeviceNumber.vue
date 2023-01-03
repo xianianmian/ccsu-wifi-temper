e8041e32a1f76aae2a596a121d5cf4c2e7d8b5b2
mayday831 <2438008482@qq.com>
2023-01-03 17:06:45 +0800

    进行了修改

--- src/views/dashboard/DeviceNumber.vue
+++ src/views/dashboard/DeviceNumber.vue
@@ -6,25 +6,44 @@
         电解槽总运行数量
       </div>
       <div style="color: #5dbec8; font-size: 20px">
-        <span style="font-size: 45px">12</span>个
+        <span style="font-size: 45px">{{total}}</span>个
       </div>
-      <div style="color: #7a9064; font-size: 20px">安全运行率：100%</div>
+      <div style="color: #7a9064; font-size: 20px">安全运行率：{{safePercent}}%</div>
     </div>
   </div>
 </template>
 
 <script>
+import { listCellError } from "@/api/fac/cellError.js";
 export default {
   data() {
     return {
       dateTime: "",
+      total:4,
+      safePercent:1,
+      timer:null,
     };
   },
   mounted() {
     setInterval(() => {
       this.dateTime = this.parseTime(new Date(), "{y}-{m}-{d} {h}:{i}:{s}");
     }, 1000);
+    // 启动时自动调用一次获取相关信息
+    this.getListCellError();
+    // 每隔30分钟获取一次数据
+    this.timer=setInterval(()=>{
+      this.getListCellError()
+    },1000*60*30);
   },
+  methods:{
+    getListCellError() {
+      listCellError().then((res) => {
+        console.log(res.data);
+        this.total=res.data.all;
+        this.safePercent = (res.data.normal / res.data.all).toFixed(0);
+      });
+    },
+  }
 };
 </script>
 
